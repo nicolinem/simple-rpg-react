e78333a67f449b35c3a98aff4c841529316b6fc3
Nicoline Mork <nicolimo@stud.ntnu>
2022-07-09 11:03:34 +0200

    extract all drawing to GameMap class

---
+++ src/GameMap.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from "react";
+import Canvas from "./Canvas";
+import useMap from "./useMap";
+import topMap from "./images/maps/DemoUpper.png";
+import heroImage from "./images/characters/people/hero.png";
+import { store } from "./store.js";
+
+const GameMap = (props) => {
+  //   const globalState = useContext(store);
+  //   console.log(globalState.state);
+  //   console.log(globalState);
+  const { mapID, x, y, position } = props;
+  const [hero, setHero] = useState(null);
+
+  const { topImage, bottomImage } = useMap(mapID);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = heroImage;
+    image.onload = () => setHero(image);
+    // console.log(globalState);
+  }, [position]);
+
+  const draw = (ctx, frameCount) => {
+    if (bottomImage) {
+      ctx.clearRect(0, 0, 1056, 594);
+      ctx.drawImage(bottomImage, 0, 0);
+      ctx.drawImage(
+        hero,
+        0,
+        0,
+        32,
+        32,
+        position.x * 16 - 8,
+        position.y * 16 - 2,
+        32,
+        32
+      );
+
+      ctx.drawImage(topImage, 0, 0);
+    }
+  };
+
+  return <Canvas draw={draw}></Canvas>;
+};
+
+export default GameMap;
