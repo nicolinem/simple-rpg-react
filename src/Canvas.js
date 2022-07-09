a77bc2f652c1a5609b84401a843cc0271ef67eb1
Nicoline Mork <nicolimo@stud.ntnu>
2022-07-09 11:00:21 +0200

    add Canvas

---
+++ src/Canvas.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+import "./Canvas.css";
+import useCanvas from "./useCanvas";
+
+const Canvas = (props) => {
+  const { draw, ...rest } = props;
+
+
+  const canvasRef = useCanvas(draw);
+
+  return (
+    <div className="game-container">
+      <canvas
+        // class="game-canvas"
+        id="canvasID"
+        ref={canvasRef}
+        width="353"
+        height="198"
+      ></canvas>
+    </div>
+  );
+};
+
+export default Canvas;
