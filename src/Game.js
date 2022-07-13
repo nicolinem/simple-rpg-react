1c045b3ff453845076d613bd8807775637e856cd
Nicoline Mork <nicolimo@stud.ntnu>
2022-07-13 05:44:26 +0200

    moved logic to App

--- src/Game.js
+++ src/Game.js
@@ -2,23 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import Canvas from "./Canvas";
 import GameMap from "./GameMap";
 import { store } from "./store.js";
-import useMovement from "./hooks/useMovement";
 
-const Game = () => {
-  const position = useMovement();
-
-  const [map, setMap] = useState("DemoRoom");
-
-  const buttonPush = () => {
-    setMap("Kitchen");
-    console.log("buttonpushed");
-    console.log(map);
-  };
-  return (
-    <>
-      <button onClick={() => buttonPush()}></button>
-      <GameMap mapID={map} position={position}></GameMap>
-    </>
-  );
-};
+const Game = () => {};
 export default Game;
